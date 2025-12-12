1301476638d3b3e7054d3b49e85b5dfaafe8533f
tessavermeulen07 <tessavermeulen@me.com>
2025-12-12 14:37:27 +0100

    opdracht 2 start

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     const [worldMap, setWorldMap] = useState([]);
     const [error, toggleError] = useState(false);
     const [loading, toggleLoading] = useState(false);
+    const [countrySearch, setCountrySearch] = useState({});
 
     async function getCountries() {
         try {
@@ -24,8 +25,6 @@ function App() {
 
             setWorldMap(result.data);
 
-
-
         } catch (error) {
             console.error(error);
             toggleError(true);
@@ -34,23 +33,62 @@ function App() {
         }
     }
 
+    async function searchCountry() {
+        try {
+            toggleLoading(true);
+            toggleError(false);
+
+            const resultSearchCountry = await axios.get('https://restcountries.com/v3.1/name/netherlands');
+            console.log(resultSearchCountry.data[0]);
+
+            setCountrySearch(resultSearchCountry.data[0]);
+        } catch (error) {
+            console.error(error);
+            toggleError(true);
+        } finally {
+            toggleLoading(false);
+        }
+    }
+
+    console.log(countrySearch)
 
     return (
         <>
-            <img src={world_map} alt="World Map" className="img-worldmap" />
+            <img src={world_map} alt="World Map" className="img-worldmap"/>
 
             <h1>World Map</h1>
+
+            <form className="form-container">
+                <label htmlFor="country">
+                    <input
+                        type="text"
+                        id="country"
+                        name="country"
+                        size="50"
+                    />
+                </label>
+                <button type="button" onClick={searchCountry} disabled={loading}>Search</button>
+            </form>
+
+            { Object.keys(countrySearch).length > 0 && <article>
+                <img src={countrySearch.flags?.svg} alt={countrySearch.flags?.alt} className="img-flag"/>
+                <h2 className={regionName(countrySearch.region)}>
+                    {countrySearch.name?.common}
+                </h2>
+                <p>{`Has a population of ${countrySearch?.population} people`}</p>
+            </article>}
 
             <div className="button-class">
                 <button type="button" onClick={getCountries} disabled={loading}>Haal landen op</button>
             </div>
+
 
             {error && <p>Er is iets mis gegaan, probeer het later nog een keer</p>}
 
             <ul className="list-item-container">
                 {worldMap.map((country) => {
                     return (<li key={country?.name?.official}>
-                        <img src={country?.flags?.svg} alt={country[0]?.flags?.alt} className="img-flag" />
+                        <img src={country?.flags?.svg} alt={country[0]?.flags?.alt} className="img-flag"/>
                         <h2 className={regionName(country?.region)}>
                             {country?.name?.common}
                         </h2>
@@ -58,7 +96,7 @@ function App() {
                     </li>)
                 })}
             </ul>
-            
+
         </>
     )
 }
